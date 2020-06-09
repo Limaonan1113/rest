54e5d6f8aaf3f46149874d924f453cc159093468
limaonan <18723834972@163.com>
2020-06-09 11:34:21 +0800

    restful接口报文头穿参数

--- src/main/java/com/restful/rest/resorce/RestfulTestContrroller.java
+++ src/main/java/com/restful/rest/resorce/RestfulTestContrroller.java
@@ -1,8 +1,10 @@
 package com.restful.rest.resorce;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -18,6 +20,12 @@ public class RestfulTestContrroller {
         String a="SB";
         Map b= new HashMap();
         b.put("xgc",a);
+        return b;
+    }
+    @GetMapping("/userlistParam")
+    public Map userlistParam(@RequestParam(required = true)String xgc){
+        Map b = new HashMap();
+        b.put("xgc",xgc);
         return b;
     }
 
